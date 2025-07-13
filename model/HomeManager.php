8536fcf8ec4514f9776473a6f01251ba7a6004ed
CLaireLise-dev <clairedemettre@gmail.com>
2025-07-13 11:44:46 +0200

    Affichage des projets par ordre desc

--- model/HomeManager.php
+++ model/HomeManager.php
@@ -12,21 +12,21 @@ class HomeManager extends Manager
     public function getProjects()
     {
         $bdd = $this->getPDO();
-        $requete = $bdd->query('SELECT * FROM projects');
+        $requete = $bdd->query('SELECT * FROM projects ORDER BY id DESC');
         return $requete->fetchAll(PDO::FETCH_ASSOC);
     }
 
     public function getArticles()
     {
         $bdd = $this->getPDO();
-        $requete = $bdd->query('SELECT * FROM articles');
+        $requete = $bdd->query('SELECT * FROM articles ORDER BY id DESC');
         return $requete->fetchAll(PDO::FETCH_ASSOC);
     }
 
     public function getWordpress()
     {
         $bdd = $this->getPDO();
-        $requete = $bdd->query('SELECT * FROM wordpress');
+        $requete = $bdd->query('SELECT * FROM wordpress ORDER BY id DESC');
         return $requete->fetchAll(PDO::FETCH_ASSOC);
     }
 }
